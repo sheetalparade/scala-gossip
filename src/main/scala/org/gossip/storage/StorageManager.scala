1bca1883fdc1df975672a4bd6a96bad64fc3b9e2
Sheetal Parade <sheetal.parade+github@gmail.com>
2015-10-03 21:42:34 -0400

    moved WorkerHandler to StorageManager

--- src/main/scala/org/gossip/storage/StorageManager.scala
+++ src/main/scala/org/gossip/storage/StorageManager.scala
@@ -1,37 +1,68 @@
 package org.gossip.storage
 
 import scala.collection.mutable.ListBuffer
+import java.nio.ByteBuffer
+import org.gossip.network.actors.WorkerHandler
 
 /**
  * @author sparade
  */
 
 object StorageManager {
-  
-  
-  
-  def getVersions (namespace: String):Map[String, Int] = {
+
+  def getVersions(namespace: String): Map[String, Int] = {
     null;
   }
-  
-  def save(namespace: String, keys: Array[String]) : Data = {
+
+  def save(namespace: String, keys: Array[String]): Data = {
     null;
   }
-  
-  def checkVersionsDiff(namespace: String, iVersions: Map[String, Int]) : Map[String, Int] = {
+
+  def checkVersionsDiff(namespace: String, iVersions: Map[String, Int]): Map[String, Int] = {
     null;
+  }
+
+  /**
+   * Implementation of WorkerActor
+   *
+   * StorageWorkers handles all incoming request from the server.
+   * This class is aware of all storage needs. All writes to
+   * Storage should be handles through this Actor to avoid
+   * data concurrency issues.
+   *
+   * Reads to Data could be direct access.
+   *
+   *
+   * @author sparade
+   */
+  class StorageWorkerActor extends WorkerHandler {
+
+    override def handleRemoteMessage(data: ByteBuffer): ByteBuffer = {
+      return null;
+    }
+
+    override def firstMessage: ByteBuffer = {
+      return null;
+    }
+
+    /**
+     * All data writes will be handled over actor messaging with this call.
+     */
+    override def handleStorageMessage(data: Any) {
+    }
+
   }
 }
 
 class Data {
-  
+
   private var dataMap = ListBuffer[Record]()
-  
+
   def save = {}
-  
+
   def update = {}
-  
+
   def get = dataMap
-  
-  case class Record (namespace: String, version: Int, bytes: Array[Byte]);
+
+  case class Record(namespace: String, version: Int, bytes: Array[Byte]);
 }
