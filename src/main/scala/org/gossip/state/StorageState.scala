56211299417dc23479b2a0bdf760bb2bea33e150
Sandeep More <moresandeep@gmail.com>
2015-10-07 13:19:17 -0500

    make the state maps mutable

--- src/main/scala/org/gossip/state/StorageState.scala
+++ src/main/scala/org/gossip/state/StorageState.scala
@@ -3,47 +3,55 @@ import java.net.InetAddress
 
 /**
  * Manages the state of data received from various agents on the cluster.
- * 
+ *
  */
 trait StorageState {
-  
+
 }
 
 /**
- * An in-memory implementation 
+ * An in-memory implementation
  * of the storage component.
  */
 class MemoryStorage extends StorageState {
-  
+
   /**
    * Stores the local (known) state of endpoints.
    */
-  private var endpointStateMap = Map[InetAddress, NodeState]()
-  
+  private var endpointStateMap = scala.collection.mutable.Map[InetAddress, NodeState]()
+
   /**
    * A list of live endpoints
    */
   private var liveEndpoints = List[InetAddress]()
-  
+
   /**
    * A list of dead endpoints with timestamp
    */
-  private var deadEndpoints = Map[InetAddress, Long]()
-  
+  private var deadEndpoints = scala.collection.mutable.Map[InetAddress, Long]()
+
   /**
    * A map of endpoints that was removed from gossip.
    * Any gossip for these endpoints will be ignored
    */
-  private var justRemovedEndpoints = Map[InetAddress, Long]()
-  
+  private var justRemovedEndpoints = scala.collection.mutable.Map[InetAddress, Long]()
+
   /**
    * A map of endpoints that are expired.
    */
-  private var expiredEndpoints = Map[InetAddress, Long]()
-  
+  private var expiredEndpoints = scala.collection.mutable.Map[InetAddress, Long]()
+
   /**
-   * A map of endpoint versions. 
+   * A map of endpoint versions.
    * Let's know which endpoint is on what version.
    */
-  private var endpointVersions = Map[InetAddress, Integer]()
+  private var endpointVersions = scala.collection.mutable.Map[InetAddress, Integer]()
+}
+
+/**
+ * A companion object for the calas {@link MemoryStorage} that contains
+ * utility methods.
+ */
+object MemoryStorage {
+
 }
